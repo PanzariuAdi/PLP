5fe9dcae93aa4e72eaaa6bcc1295a52ef6d4c34b
PanzariuAdi <panzariu.adi00@gmail.com>
2020-12-13 14:32:32 +0200

    Refactoring & vector declarations.

--- proiect.v
+++ proiect.v
@@ -54,41 +54,70 @@ Inductive Stmt :=
   | ifthen : BExp -> Stmt -> Stmt
   | switch : AExp -> Stmt.
 
-Notation "A => switch" := (switch A) (at level 31, right associativity).
-Check 2 => switch.
+Inductive LstElement :=
+  | natElem: AExp -> LstElement
+  | stringElem: string -> LstElement
+  | boolElem: BExp -> LstElement
+  | lstSequence : LstElement -> LstElement -> LstElement.
+
+Coercion natElem: AExp >-> LstElement.
+Coercion stringElem : string >-> LstElement.
+Coercion boolElem : BExp >-> LstElement.
 
 Inductive Vector :=
-  | vector_decl : string -> Stmt -> Vector
-  | vector_assign : string -> Stmt -> Vector.
+  | vector_decl : string -> LstElement -> Vector
+  | vector_assign : string -> LstElement -> Vector.
 
 Inductive Define :=
   | define_nat : AExp -> string -> Define
   | define_string : string -> string -> Define.
 
-Notation "A #defineNat B" := (define_nat A B) (at level 50). 
-Notation "A #defineString B" := (define_string A B) (at level 50).
-
-Check 15 #defineNat "maxPoints".
-Check "a" #defineString "b".
 
 Inductive FuncAExp :=
   | amax: AExp -> AExp -> FuncAExp
   | amin: AExp -> AExp -> FuncAExp
   | apow: AExp -> AExp -> FuncAExp.
 
-Notation "A maxim? B" := (amax A B) (at level 60).
-Notation "A minim? B" := (amin A B) (at level 60).
-Notation "A puterea? B" := (apow A B) (at level 60).
-
-Check 1 maxim? 2.
-Check 1 minim? 2.
-Check 2 puterea? 3.
-
 Inductive BreakContinue :=
   | break : BreakContinue
   | continue : BreakContinue.
 
+(*Predefined functions.*)
+Notation "'fMaxim' A ~ B" := (amax A B) (at level 60).
+Notation "'fMinim' A ~ B" := (amin A B) (at level 60).
+Notation "'fPow' A ~ B" := (apow A B) (at level 60).
+
+(*Break & Continue*)
 Notation "Break!" := (break) (at level 50).
 Notation "Continue!" := (continue) (at level 50).
 
+(*Define*)
+Notation "A #defineNat B" := (define_nat A B) (at level 50). 
+Notation "A #defineString B" := (define_string A B) (at level 50).
+
+(*Switch*)
+Notation "'fSwitch' A" := (switch A) (at level 30, right associativity).
+
+(*Declarations*)
+Notation "'iNat' X ::= A" := (nat_decl X A)(at level 90).
+Notation "'iBool' X ::= A" := (bool_decl X A)(at level 90).
+Notation "'iVector' X ::= A " := (vector_decl X A)(at level 90).
+
+(*Assigns*)
+Notation "X :n= A" := (nat_assign X A)(at level 90).
+Notation "X :b= A" := (bool_assign X A)(at level 90).
+Notation "X :v= A" := (vector_assign X A)(at level 90).
+
+Notation "S1 ;; S2" := (sequence S1 S2) (at level 93, right associativity).
+Notation "S1 .-> S2" := (lstSequence S1 S2) (at level 93, right associativity).
+
+Check iVector "fullName" ::= ("Panzariu" .-> "Ionut" .-> "Adrian").
+Check iVector "numbers" ::= (1 .-> 2 .-> 3).
+Check fMaxim 1 ~ 2.
+Check fMinim 1 ~ 2.
+Check fPow 1 ~ 2.
+Check fSwitch 2.
+Check 15 #defineNat "maxPoints".
+Check "a" #defineString "b".
 Check Break!.
+Check Continue!.
